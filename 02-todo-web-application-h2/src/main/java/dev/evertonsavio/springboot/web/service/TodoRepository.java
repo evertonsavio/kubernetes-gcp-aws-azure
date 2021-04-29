c068b10a91b6632bbe039e57d851b88be912aca7
Havyx <everluca@outlook.com>
2021-04-29 15:20:03 -0300

    :tada: todo h2 war build

---
+++ 02-todo-web-application-h2/src/main/java/dev/evertonsavio/springboot/web/service/TodoRepository.java
@@ -0,0 +1,11 @@
+package dev.evertonsavio.springboot.web.service;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.evertonsavio.springboot.web.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+	List<Todo> findByUser(String user);
+}
